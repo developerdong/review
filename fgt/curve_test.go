471f122a7ece16fb5dec9568999b1be2e63476f1
Zhibin Dong <developerdong@gmail.com>
2021-08-28 21:17:45 +0800

    refactor: make the tests better

--- fgt/curve_test.go
+++ fgt/curve_test.go
@@ -7,10 +7,12 @@ import (
 )
 
 func TestGetRetrievability(t *testing.T) {
-	point0 := time.Unix(0, 0)
-	point1 := time.Unix(20*24*3600, 0)
+	points := []time.Time{
+		time.Unix(0, 0),
+		time.Unix(20*24*3600, 0),
+	}
 	now := time.Unix(32*24*3600, 0)
-	if result := GetRetrievability([]time.Time{point0, point1}, now); !(math.Abs(result-0.54881) < 1e-5) {
+	if result := GetRetrievability(points, now); !(math.Abs(result-0.54881) < 1e-5) {
 		t.Error(result)
 	} else {
 		t.Log(result)
@@ -24,9 +26,25 @@ func TestGetRetrievability2(t *testing.T) {
 }
 
 func TestGetRetrievability3(t *testing.T) {
-	point := time.Unix(0, 0)
+	points := []time.Time{
+		time.Unix(0, 0),
+	}
 	now := time.Unix(32*24*3600, 0)
-	if result := GetRetrievability([]time.Time{point}, now); !(math.Abs(result-0.20190) < 1e-5) {
+	if result := GetRetrievability(points, now); !(math.Abs(result-0.20190) < 1e-5) {
+		t.Error(result)
+	} else {
+		t.Log(result)
+	}
+}
+
+func TestGetRetrievability4(t *testing.T) {
+	points := []time.Time{
+		time.Unix(0, 0),
+		time.Unix(10*24*3600, 0),
+		time.Unix(20*24*3600, 0),
+	}
+	now := time.Unix(30*24*3600, 0)
+	if result := GetRetrievability(points, now); !(math.Abs(result-0.60653) < 1e-5) {
 		t.Error(result)
 	} else {
 		t.Log(result)
