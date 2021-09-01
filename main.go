f6a8bb79067da2b4aa990131a8239e5159738f84
Zhibin Dong <developerdong@gmail.com>
2021-09-01 19:17:56 +0800

    feat: add an entrypoint

---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"github.com/developerdong/review/conf"
+	"github.com/developerdong/review/sql"
+	"net/url"
+	"os"
+)
+
+const help = `
+usage:
+	review <insert|select|delete> [url]
+example:
+	review insert https://www.google.com
+	review select
+	review delete https://www.google.com
+`
+
+func main() {
+	// init the storage instance
+	var storage sql.Storage
+	switch driverName := conf.GetEnv(conf.DriverName); driverName {
+	case sql.SqliteDriverName:
+		storage = new(sql.Sqlite)
+	default:
+		fmt.Printf("the driver name %s is unsupported\n", driverName)
+		os.Exit(1)
+	}
+	// execute commands
+	if !((len(os.Args) == 2 && os.Args[1] == "select") || (len(os.Args) == 3 && (os.Args[1] == "insert" || os.Args[1] == "delete"))) {
+		// the format of input is incorrect
+		fmt.Print(help)
+		os.Exit(1)
+	} else {
+		// process different command
+		switch os.Args[1] {
+		case "insert":
+			if u, err := url.Parse(os.Args[2]); err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			} else if err := storage.Insert(u); err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+		case "select":
+			if u, err := storage.Select(); err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			} else {
+				fmt.Println(u.String())
+			}
+		case "delete":
+			if u, err := url.Parse(os.Args[2]); err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			} else if err := storage.Delete(u); err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+		}
+	}
+}
