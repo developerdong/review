2715d7ff6d2ab4941ae0f2cfb35ccbdf716ba5a5
Zhibin Dong <developerdong@gmail.com>
2021-09-06 15:26:29 +0800

    feat: new formula

--- fgt/curve.go
+++ fgt/curve.go
@@ -9,13 +9,30 @@ import (
 	"time"
 )
 
-// The stability of memory, which makes exp(-t/S)≈0.2 when t is 32 days.
-const stability = 20 * 24 * time.Hour
+const (
+	// The self-defined forgetting factor.
+	k = 1 / (3 * math.E * float64(24*time.Hour))
+	// The final retrievability of memory when t➡+∞
+	r = 0.2
+)
 
-// getRetrievability returns R=exp(-t/S), where `R` is the retrievability of memory, `t` is time, and `S` is the stability of memory.
-// Ref: Trahan, Donald E. ; Larrabee, Glenn J. (1995). Effect of normal aging on rate of forgetting. Neuropsychology 6. 2 : 115-122.
+// According to the Newton's Law of Cooling, T=H+(T0-H)*exp(-k(t-t0)), where
+// T: the temperature of thing at `t`
+// H: the ambient temperature
+// T0: the temperature of thing at `t0`
+// k: the cooling factor
+// t: the current time
+// t0: the initial time
+// Similarly, the forgetting curve follows this formula.
+// We can use `T` as the retrievability of memory at `t`,
+// `H` as the final retrievability of memory,
+// `T0` as the retrievability of memory at `t0`,
+// and `k` as the forgetting factor.
+// According to the data published by Ebbinghaus, let H=0.2, and t0=0,
+// then of course T0 is 1, so the forgetting formula is:
+// T=0.2+0.8*exp(-kt)
 func getRetrievability(duration time.Duration) float64 {
-	return math.Exp(-float64(duration) / float64(stability))
+	return r + (1-r)*math.Exp(-k*float64(duration))
 }
 
 // GetRetrievability returns the remaining retrievability of knowledge until now after some time points of learning.
--- fgt/curve_test.go
+++ fgt/curve_test.go
@@ -6,47 +6,68 @@ import (
 	"time"
 )
 
+// The retrievability at time 0 should be 1.
 func TestGetRetrievability(t *testing.T) {
 	points := []time.Time{
 		time.Unix(0, 0),
-		time.Unix(20*24*3600, 0),
 	}
-	now := time.Unix(32*24*3600, 0)
-	if result := GetRetrievability(points, now); !(math.Abs(result-0.54881) < 1e-5) {
+	now := time.Unix(0, 0)
+	if result := GetRetrievability(points, now); result != 1 {
 		t.Error(result)
 	} else {
 		t.Log(result)
 	}
 }
 
+// The retrievability should not be smaller than `r`.
+func TestGetRetrievability1(t *testing.T) {
+	points := []time.Time{
+		time.Unix(0, 0),
+	}
+	now := time.Unix(math.MaxUint32, 0)
+	if result := GetRetrievability(points, now); result >= r {
+		t.Log(result)
+	} else {
+		t.Error(result)
+	}
+}
+
+// The retrievability should be 0 if we have never learned it.
 func TestGetRetrievability2(t *testing.T) {
 	if result := GetRetrievability(nil, time.Now()); result != 0 {
 		t.Error(result)
 	}
 }
 
+// The retrievability computed by this program should be equal to that computed by hand.
 func TestGetRetrievability3(t *testing.T) {
 	points := []time.Time{
 		time.Unix(0, 0),
 	}
-	now := time.Unix(32*24*3600, 0)
-	if result := GetRetrievability(points, now); !(math.Abs(result-0.20190) < 1e-5) {
+	now := time.Unix(18*24*3600, 0)
+	if result := GetRetrievability(points, now); !(math.Abs(result-0.288) < 1e-5) {
 		t.Error(result)
 	} else {
 		t.Log(result)
 	}
 }
 
+// The later we review, the better the result is.
 func TestGetRetrievability4(t *testing.T) {
-	points := []time.Time{
+	points1 := []time.Time{
 		time.Unix(0, 0),
 		time.Unix(10*24*3600, 0),
 		time.Unix(20*24*3600, 0),
 	}
+	points2 := []time.Time{
+		time.Unix(0, 0),
+		time.Unix(10*24*3600, 0),
+		time.Unix(20*24*3600+1, 0),
+	}
 	now := time.Unix(30*24*3600, 0)
-	if result := GetRetrievability(points, now); !(math.Abs(result-0.60653) < 1e-5) {
-		t.Error(result)
+	if result1, result2 := GetRetrievability(points1, now), GetRetrievability(points2, now); !(result1 < result2) {
+		t.Error(result1, result2)
 	} else {
-		t.Log(result)
+		t.Log(result1, result2)
 	}
 }
