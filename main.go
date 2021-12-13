aa31a0cbde158cd5cba39e39f03abb191f940d49
Zhibin Dong <developerdong@gmail.com>
2021-12-13 10:37:15 +0800

    refactor: use functions to print and exit

--- main.go
+++ main.go
@@ -18,6 +18,21 @@ example:
 	review delete https://www.google.com
 `
 
+func Fatal(v ...interface{}) {
+	fmt.Print(v...)
+	os.Exit(1)
+}
+
+func Fatalf(format string, v ...interface{}) {
+	fmt.Printf(format, v...)
+	os.Exit(1)
+}
+
+func Fatalln(v ...interface{}) {
+	fmt.Println(v...)
+	os.Exit(1)
+}
+
 func main() {
 	// init the storage instance
 	var storage sql.Storage
@@ -25,42 +40,34 @@ func main() {
 	case sql.SqliteDriverName:
 		storage = new(sql.Sqlite)
 	case "":
-		fmt.Println("the name of sql driver should be set in the environment")
-		os.Exit(1)
+		Fatalln("the name of sql driver should be set in the environment")
 	default:
-		fmt.Printf("the driver name %s is unsupported\n", driverName)
-		os.Exit(1)
+		Fatalf("the driver name %s is unsupported\n", driverName)
 	}
 	// execute commands
 	if !((len(os.Args) == 2 && os.Args[1] == "select") || (len(os.Args) == 3 && (os.Args[1] == "insert" || os.Args[1] == "delete"))) {
 		// the format of input is incorrect
-		fmt.Print(help)
-		os.Exit(1)
+		Fatal(help)
 	} else {
 		// process different command
 		switch os.Args[1] {
 		case "insert":
 			if u, err := url.Parse(os.Args[2]); err != nil {
-				fmt.Println(err)
-				os.Exit(1)
+				Fatalln(err)
 			} else if err := storage.Insert(u); err != nil {
-				fmt.Println(err)
-				os.Exit(1)
+				Fatalln(err)
 			}
 		case "select":
 			if u, r, err := storage.Select(); err != nil {
-				fmt.Println(err)
-				os.Exit(1)
+				Fatalln(err)
 			} else {
 				fmt.Println(u.String(), r)
 			}
 		case "delete":
 			if u, err := url.Parse(os.Args[2]); err != nil {
-				fmt.Println(err)
-				os.Exit(1)
+				Fatalln(err)
 			} else if err := storage.Delete(u); err != nil {
-				fmt.Println(err)
-				os.Exit(1)
+				Fatalln(err)
 			}
 		}
 	}
