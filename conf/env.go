f0086af7acc2bdc97418f646500df4b0cab8a963
Zhibin Dong <developerdong@gmail.com>
2022-09-03 08:39:04 +0000

    fix: weak warnings of comment

--- conf/env.go
+++ conf/env.go
@@ -7,10 +7,10 @@ type env string
 const prefix env = "REVIEW_"
 
 const (
-	// See https://pkg.go.dev/database/sql#Open.
+	// DriverName is from https://pkg.go.dev/database/sql#Open.
 	DriverName     = prefix + "DRIVER_NAME"
 	DataSourceName = prefix + "DATA_SOURCE_NAME"
-	// The path of file whose content is the previously selected url.
+	// URLFilePath is the path of file whose content is the previously selected url.
 	URLFilePath = prefix + "URL_FILE_PATH"
 )
 
